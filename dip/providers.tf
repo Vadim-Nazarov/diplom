78f88416d9784bab00ce473346584d9bcc01561d
Vadim-Nazarov <107613708+Vadim-Nazarov@users.noreply.github.com>
2024-10-25 15:10:47 +0300

    Update providers.tf

--- dip/providers.tf
+++ dip/providers.tf
@@ -1,3 +1,4 @@
+  GNU nano 7.2                                                                          providers.tf
 terraform {
   required_providers {
     yandex = {
@@ -5,8 +6,22 @@ terraform {
     }
   }
   required_version = ">=0.13"
-}
 
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "nazarovdiplom-bucket-2024"
+    region = "ru-central1"
+    key    = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+
+  }
+}
 # Блок конфигурации провайдера Yandex.Cloud
 provider "yandex" {
   token     = var.token
@@ -17,3 +32,5 @@ provider "yandex" {
 data "yandex_compute_image" "public-ubuntu" {
   image_id = var.public_image
 }
+
+
